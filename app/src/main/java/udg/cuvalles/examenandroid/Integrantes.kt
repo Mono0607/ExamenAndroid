9e15120488bbefc1f6330c993eaec217b6b35b7d
Mono0607 <lalo_mono0607@outlook.com>
2022-05-09 22:15:16 -0500

    Se agrego una activity y se modificaron las vistas

---
+++ app/src/main/java/udg/cuvalles/examenandroid/Integrantes.kt
@@ -0,0 +1,11 @@
+package udg.cuvalles.examenandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Integrantes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_integrantes)
+    }
+}
