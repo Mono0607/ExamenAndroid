3ceb774da48781381a4874082c271685088c04a5
Mono0607 <lalo_mono0607@outlook.com>
2022-05-10 00:21:37 -0500

    se agrego la actiivity de los integrantes

--- app/src/main/java/udg/cuvalles/examenandroid/Integrantes.kt
+++ app/src/main/java/udg/cuvalles/examenandroid/Integrantes.kt
@@ -1,11 +1,22 @@
 package udg.cuvalles.examenandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class Integrantes : AppCompatActivity() {
+    lateinit var btnReturn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_integrantes)
+
+        btnReturn = findViewById(R.id.btnReturn)
+
+        btnReturn.setOnClickListener {
+            val intentReturn = Intent(this, MainActivity::class.java)
+            startActivity(intentReturn)
+        }
     }
 }
